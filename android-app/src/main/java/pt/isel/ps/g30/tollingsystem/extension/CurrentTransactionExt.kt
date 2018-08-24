5dad6b663ba3cd942fa1553089dda6cd3b388b17
0rp3u <david_mfra@hotmail.com>
2018-08-24 18:05:15 +0100

    added extension methods to help map transactions and trips

---
+++ android-app/src/main/java/pt/isel/ps/g30/tollingsystem/extension/CurrentTransactionExt.kt
@@ -0,0 +1,11 @@
+package pt.isel.ps.g30.tollingsystem.extension
+
+import pt.isel.ps.g30.tollingsystem.data.database.model.CurrentTransaction
+import pt.isel.ps.g30.tollingsystem.data.database.model.TollingTransaction
+
+fun CurrentTransaction.toFInishedTransaction(): TollingTransaction{
+    if(vehicle == null || destination == null || destTimestamp == null || origin ==null || originTimestamp == null) throw Exception(" could not convert to tolling transaction, active transaction is not finished")
+
+    return TollingTransaction(vehicle!!, origin!!, originTimestamp!!, destination!!, destTimestamp!!)
+
+}
