99a14f2dab09e5a9d62edb1ca7fa8cc19b9ba098
jpedroduarte <redice_dragon@hotmail.com>
2018-09-03 16:19:21 +0100

    Fixed bug with Transaction Enum value.
    Implemented Vehicle Fetch with Date filter.

--- server/src/main/kotlin/pt/isel/ps/LI61N/g30/server/model/domain/repositories/VehicleRepository.kt
+++ server/src/main/kotlin/pt/isel/ps/LI61N/g30/server/model/domain/repositories/VehicleRepository.kt
@@ -1,7 +1,9 @@
 package pt.isel.ps.LI61N.g30.server.model.domain.repositories
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import pt.isel.ps.LI61N.g30.server.model.domain.Event
 import pt.isel.ps.LI61N.g30.server.model.domain.User
 import pt.isel.ps.LI61N.g30.server.model.domain.Vehicle
 import java.util.*
@@ -11,4 +13,7 @@ interface VehicleRepository : CrudRepository<Vehicle, Long> {
 
    fun findByOwner(user: User): List<Vehicle>
    fun findByOwnerAndId(user: User, id: Long): Optional<Vehicle>
+
+    @Query("select u from Vehicle u where u.updated > ?1 and u.owner = ?2")
+   fun findByUpdatedAfterAndOwner(date: Date, user: User): List<Vehicle>
 }
